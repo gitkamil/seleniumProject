96053d1431fd675bac9c5a34b8aa58e10e3c35a7
Kamil <kamilworona@gmail.com>
2020-02-08 23:18:28 +0100

    add page object pattern

--- src/test/java/tests/myTestSelenium.java
+++ src/test/java/tests/myTestSelenium.java
@@ -7,42 +7,72 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.Login;
+import pages.StartPage;
 
 public class myTestSelenium {
-//how to create correct profile https://stackoverflow.com/questions/50635087/how-to-open-a-chrome-profile-through-user-data-dir-argument-of-selenium
+    //how to create correct profile https://stackoverflow.com/questions/50635087/how-to-open-a-chrome-profile-through-user-data-dir-argument-of-selenium
 // profil will works only when all instances of chrome will be closed
     @Test
     public void testGoogleSearch() throws InterruptedException {
         // Optional. If not specified, WebDriver searches the PATH for chromedriver.
-       System.setProperty("webdriver.chrome.driver", "C:\\Users\\woronkam\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamil\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
         ChromeOptions chOption = new ChromeOptions();
-        chOption.addArguments("user-data-dir=C:\\Users\\woronkam\\AppData\\Local\\Google\\Chrome\\User Data");
-        chOption.addArguments("profile-directory=Profile 3");
-        chOption.addArguments("--disable-extensions");
+//        chOption.addArguments("user-data-dir=C:\\Users\\Kamil\\AppData\\Local\\Google\\Chrome\\User Data");
+//        chOption.addArguments("profile-directory=Profile 3");
+//        chOption.addArguments("--disable-extensions");
         chOption.addArguments("--start-maximized");
 
         WebDriver driver = new ChromeDriver(chOption);
-    //    driver.manage().window().maximize();
-    //    JavascriptExecutor js = (JavascriptExecutor) driver;
+        //    driver.manage().window().maximize();
+        //    JavascriptExecutor js = (JavascriptExecutor) driver;
 
 
         driver.get("https://github.com/");
         Thread.sleep(5000);  // Let the user actually see something!
         WebElement logInLink = driver.findElement(By.linkText("Sign in"));
         logInLink.click();
-     //   WebElement button = driver.findElement(By.className("ws__skipButton"));
- //       button.click();
+//        WebElement button = driver.findElement(By.className("ws__skipButton"));
+//        button.click();
+//
+        WebElement searchBox = driver.findElement(By.name("login"));
+        searchBox.sendKeys("gitkamil");
+        searchBox.submit();
 
-    //    WebElement searchBox = driver.findElement(By.name("login"));
-     //   searchBox.sendKeys("gitkamil");
-       // searchBox.submit();
+        WebElement passwordBox = driver.findElement(By.name("password"));
+        passwordBox.sendKeys("Ktoscos123@");
+        passwordBox.submit();
+//
+//
+//        driver.quit();
+    }
 
-     //   WebElement passwordBox = driver.findElement(By.name("password"));
-     //   passwordBox.sendKeys("Ktoscos123@");
-      //  passwordBox.submit();
+    //https://www.pluralsight.com/guides/getting-started-with-page-object-pattern-for-your-selenium-tests
+    //https://www.guru99.com/page-object-model-pom-page-factory-in-selenium-ultimate-guide.html
+
+    @Test
+    public void testGoogleSearch2() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamil\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        ChromeOptions chOption = new ChromeOptions();
+//        chOption.addArguments("user-data-dir=C:\\Users\\Kamil\\AppData\\Local\\Google\\Chrome\\User Data");
+//        chOption.addArguments("profile-directory=Profile 3");
+//        chOption.addArguments("--disable-extensions");
+        chOption.addArguments("--start-maximized");
+
+        WebDriver driver = new ChromeDriver(chOption);
+        //    driver.manage().window().maximize();
+        //    JavascriptExecutor js = (JavascriptExecutor) driver;
 
 
-   //     driver.quit();
+        driver.get("https://github.com/");
+        StartPage startPage = new StartPage(driver);
+        Login loginPage = startPage.submit();
+
+        loginPage.setUserName("gitKamil");
+        loginPage.setPassword("Ktoscos123@");
+        loginPage.submit();
+
     }
 }
